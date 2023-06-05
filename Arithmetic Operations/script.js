5315d12f7b1c76050e3eb679bf898b34e5ff2c62
IlonaGdula <135151434+IlonaGdula@users.noreply.github.com>
2023-06-05 19:42:28 +0100

    Add files via upload

---
+++ Arithmetic Operations/script.js
@@ -0,0 +1,19 @@
+// Perform arithmetic operations and print the results
+
+// Operation a: -5 + 8 * 6
+let resultA = -5 + 8 * 6;
+
+// Operation b: (55+9) % 9
+let resultB = (55 + 9) % 9;
+
+// Operation c: 20 + -3*5 / 8
+let resultC = 20 + (-3 * 5) / 8;
+
+// Operation d: 5 + 15 / 3 * 2 - 8 % 3
+let resultD = 5 + (15 / 3) * 2 - (8 % 3);
+
+// Print the results
+console.log(resultA); // 43
+console.log(resultB); // 1
+console.log(resultC); // 18.125
+console.log(resultD); // 13.0
