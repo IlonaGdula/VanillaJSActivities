0693e721089dfe5ad93099c9fb192b68a9135db6
IlonaGdula <135151434+IlonaGdula@users.noreply.github.com>
2023-06-06 18:04:34 +0100

    Add files via upload

---
+++ Vanilla JS Loops 1/script.js
@@ -0,0 +1,11 @@
+// Iterate from 0 to 15
+for (let i = 0; i <= 15; i++) {
+  // Check if the current number is even or odd
+  if (i % 2 === 0) {
+    // Display the message for even numbers
+    console.log(i + " is even");
+  } else {
+    // Display the message for odd numbers
+    console.log(i + " is odd");
+  }
+}
